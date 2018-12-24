7e0c667e639bb9e1eea1e97f193e777cf8adfd34
John McBride <jpmmcbride@gmail.com>
2018-12-23 20:17:04 -0700

    String update

---
+++ go_by_example/string-builtin.go
@@ -0,0 +1,13 @@
+package main
+
+import s "strings"
+import "fmt"
+
+var p = fmt.Println
+
+func main() {
+	p("Contains:", s.Contains("test", "es"))
+	p("Count:", s.Count("test", "t"))
+	p("HasPrefix:", s.HasPrefix("test", "te"))
+	p("HasSuffix:", s.HasSuffix("test", "st"))
+}
